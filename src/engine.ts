0851bdc5fe89c0c69b1f82489c17ce149c956f6f
Maxim Yakovlev <max.yakovlev555@gmail.com>
2021-03-16 21:28:11 +0300

    initial commit

---
+++ src/engine.ts
@@ -0,0 +1,24 @@
+export default class World {
+    public ctx: CanvasRenderingContext2D
+    public canvasEl: HTMLCanvasElement
+
+    constructor() {
+        this.canvasEl = document.getElementById('world') as HTMLCanvasElement
+        this.ctx = this.canvasEl.getContext('2d')
+
+        const { innerWidth, innerHeight } = window
+        this.canvasEl.width = innerWidth
+        this.canvasEl.height = innerHeight
+    }
+
+    public rect(x: number, y: number, size: number, color: string)
+    {
+        const { ctx } = this
+        const prevStyle = ctx.fillStyle
+        
+        ctx.fillStyle = color
+        ctx.fillRect(x, y, size, size)
+
+        ctx.fillStyle = prevStyle
+    }
+}
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import World from './engine'
+
+const world = new World()
+
+world.rect(0, 0, 110, 'red')
